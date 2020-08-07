9bbf4d6947c03929aa06e7458f98b2e1e58f0b50
HP <avinashtiwary6300@gmail.com>
2020-08-07 22:42:23 +0530

    header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Dishdetail from './Dishdetail';
 import { DISHES } from '../shared.js/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -19,18 +20,16 @@ class Main extends Component {
     render() {
         return (
             <div className="App">
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
+               <Header/>
                 <Menu dishes={this.state.dishes}
                     onClick={(dishID) => this.onDishSelect(dishID)} />
                 <Dishdetail 
                 dish={this.state.dishes.filter((dish)=> dish.id===this.state.selectedDish)[0]} />
                 {/* here the dishid is used to know which is being refered to */}
                 {/* the menu is added here and the dishes is mounted here itself so state is used else props was used */}
-                {/* the menu component is now made in the menuComponent.js file */}
+                {/* the menu component is now made in the menuComponent.js
+                 file */}
+                <Footer/>
 
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 // the app is passed to the element by the id ->root
 ReactDOM.render(<App />, document.getElementById('root'));
