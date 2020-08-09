db114fc56368b29e3caf2244eb0a2f115c2e8002
HP <avinashtiwary6300@gmail.com>
2020-08-09 11:47:24 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Main/>
-        {/* the menu is added here and the dishes is mounted here itself so state is used else props was used */}
-        {/* the menu component is now made in the menuComponent.js file */}
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+          {/* the menu is added here and the dishes is mounted here itself so state is used else props was used */}
+          {/* the menu component is now made in the menuComponent.js file */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './Dishdetail';
 import { DISHES } from '../shared.js/dishes';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { Redirect,Switch,Route } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
@@ -13,23 +15,36 @@ class Main extends Component {
             dishes: DISHES,
             selectedDish: null
         }
+      
     }
     onDishSelect(dishID) {
         this.setState({ selectedDish: dishID });
     }
+   
     render() {
+        const HomePage =() => {
+            return(
+                <Home/>
+            )
+        }
         return (
+            
             <div className="App">
-               <Header/>
-                <Menu dishes={this.state.dishes}
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to='/home' />
+                </Switch>
+                {/* <Menu dishes={this.state.dishes}
                     onClick={(dishID) => this.onDishSelect(dishID)} />
                 <Dishdetail 
-                dish={this.state.dishes.filter((dish)=> dish.id===this.state.selectedDish)[0]} />
+                dish={this.state.dishes.filter((dish)=> dish.id===this.state.selectedDish)[0]} /> */}
                 {/* here the dishid is used to know which is being refered to */}
                 {/* the menu is added here and the dishes is mounted here itself so state is used else props was used */}
                 {/* the menu component is now made in the menuComponent.js
                  file */}
-                <Footer/>
+                <Footer />
 
             </div>
         );
