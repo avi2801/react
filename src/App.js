eb034ec6db28124a18f8485341d27e6c44031f31
HP <avinashtiwary6300@gmail.com>
2020-08-15 16:16:55 +0530

    intro to redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux'
+import {ConfigureStore} from './redux/configureStore'
+
+const store=ConfigureStore();
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <div>
           <Main />
@@ -12,6 +17,7 @@ class App extends Component {
           {/* the menu component is now made in the menuComponent.js file */}
         </div>
       </BrowserRouter>
+      </Provider>
     );
   }
 }
